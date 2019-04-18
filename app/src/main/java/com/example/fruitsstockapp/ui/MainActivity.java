db6619dbbdf4b45db76a762e361ac261a19b569b
Milind Amrutkar <cssindiamilind@gmail.com>
2019-04-18 15:53:16 +0530

    Implemented add fruit using Dialog

--- app/src/main/java/com/example/fruitsstockapp/ui/MainActivity.java
+++ app/src/main/java/com/example/fruitsstockapp/ui/MainActivity.java
@@ -1,11 +1,23 @@
 package com.example.fruitsstockapp.ui;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.fruitsstockapp.R;
 import com.example.fruitsstockapp.adapters.ViewPagerAdapter;
+import com.example.fruitsstockapp.model.Fruit;
 import com.example.fruitsstockapp.viewmodel.FruitViewModel;
 import com.google.android.material.tabs.TabLayout;
+
+import org.w3c.dom.Text;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
@@ -29,12 +41,60 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        getSupportActionBar().hide();
-
         fruitViewModel = ViewModelProviders.of(this).get(FruitViewModel.class);
 
         viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(viewPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = new MenuInflater(this);
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_fruit:
+                Dialog dialog = new Dialog(this);
+                dialog.setContentView(R.layout.custom_add_fruit);
+                dialog.setTitle("Add Fruit");
+
+                EditText etFruitName = dialog.findViewById(R.id.etFruitName);
+                EditText etFruitCount = dialog.findViewById(R.id.etFruitCount);
+                Button btnAddFruit = dialog.findViewById(R.id.btnAddToStock);
+
+                btnAddFruit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        if (!TextUtils.isEmpty(etFruitName.getText().toString())
+                                && (!TextUtils.isEmpty(etFruitCount.getText().toString()))) {
+
+                            String fruitName = etFruitName.getText().toString();
+                            int fruitCount = Integer.valueOf(etFruitCount.getText().toString());
+
+                            if (fruitCount <= 50) {
+                                Fruit fruit = new Fruit();
+                                fruit.setFruitName(fruitName);
+                                fruit.setInStockCount(fruitCount);
+                                fruitViewModel.insert(fruit);
+                                dialog.dismiss();
+                            } else {
+                                Toast.makeText(MainActivity.this, "Fruits quantity cannot be greater than 50", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(MainActivity.this, "Fruit Name & Count cannot be empty", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+                dialog.show();
+        }
+        return super.onOptionsItemSelected(item);
+
     }
 }
